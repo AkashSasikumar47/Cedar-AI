2e9a612910b1e8bcf69303e12782d31c804a7965
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-08-28 01:39:12 +0530

    Added process-contact-form.php

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -31,3 +31,24 @@ gsap.timeline({
 })
     .from(".about-content", { opacity: 0, x: -50, duration: 1 })
     .from(".about-image", { opacity: 0, x: 50, duration: 1 }, "-=1");
+
+// Contact Me
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('contact-modal');
+    const openModalButton = document.querySelector('.open-modal');
+    const closeModalButton = document.querySelector('.close');
+
+    openModalButton.addEventListener('click', function () {
+        modal.style.display = 'block';
+    });
+
+    closeModalButton.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
