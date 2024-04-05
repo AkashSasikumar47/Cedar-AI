d7355463d78ff5765bf8a7a2d1ed64aed12317f8
AkashSasikumar47 <akashsasikumar47@gmail.com>
2024-04-05 23:53:51 +0530

    Updated content

--- app/components/Projects/Projects.tsx
+++ app/components/Projects/Projects.tsx
@@ -10,12 +10,11 @@ const Projects = () => {
                         <div className="grid grid-cols-1 gap-6 sm:grid-cols-1">
                             <a href="#" className="group relative flex aspect-square items-end overflow-hidden rounded-4xl p-4 shadow-lg">
                                 <img
-                                    src="https://images.unsplash.com/photo-1620243318482-fdd2affd7a38?auto=format&q=75&fit=crop&w=750"
+                                    src="/img/project/Project_1.png"
                                     loading="lazy"
                                     alt="Photo by Fakurian Design"
-                                    className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+                                    className="absolute inset-0 w-full h-full object-cover object-center transition duration-200 group-hover:scale-110"
                                 />
-                                <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50" />
                                 <div className="px-2 pb-2 sm:px-0 sm:pb-0 relative flex flex-col">
                                     <span className="text-gray-300">Home</span>
                                     <span className="text-lg font-semibold text-white lg:text-xl">Decoration</span>
